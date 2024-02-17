92f36543da1f7d0581dbbb32da3f82f52de89fbf
Jiakai Peng <94138989+JK1Peng@users.noreply.github.com>
2024-02-17 18:39:37 -0500

    Delete frontend/name.py

--- frontend/name.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# 指定你想列出文件的目录
-directory = "C:\\Users\\ICE Terminal Tech\\Desktop\\git\\TFT_recorder\\frontend\\portal_icon"
-
-# 遍历该目录
-for filename in os.listdir(directory):
-    if os.path.isfile(os.path.join(directory, filename)):
-        print(filename)
