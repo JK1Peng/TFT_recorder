19cd057c1abd766f2d428c38fad42483e05ca325
Jackai <jackpeng2012@126.com>
2024-02-24 03:48:36 -0500

    another  try

--- backend/src/function.py
+++ backend/src/function.py
@@ -7,16 +7,18 @@ class Function:
 
     def transform_data(self, data):
         new_data = {
-            "players": {
-                "player1": {"champions": [], "hexes": []},
-                "player2": {"champions": [], "hexes": []}
-            },
+            "players": {},
             "portal": data.get("portal1"),
             "result": data.get("btnradio"),
             "stage": data.get("stage")
         }
 
+        # 初始化玩家数据结构
+        for player_id in ['player1', 'player2']:
+            new_data["players"][player_id] = {"champions": [], "hexes": []}
+
         for key, value in data.items():
+            # 处理选手的英雄信息
             if 'player' in key and 'champion' in key:
                 player_id, _, champion_id = key.rsplit('-', 2)
                 champion_info = {
@@ -30,14 +32,13 @@ class Function:
                     ]
                 }
                 new_data["players"][player_id]["champions"].append(champion_info)
+            # 处理hex值，确保它们正确地分配给相应的玩家
             elif 'hex' in key:
-                player_id = key.split('-')[0]
-                if player_id in new_data["players"]:
-                    new_data["players"][player_id]["hexes"].append(value)
+                hex_number, hex_id = key.split('-')[1:]
+                player_id = 'player1' if hex_number == '1' else 'player2'
+                new_data["players"][player_id]["hexes"].append(value)
 
         return new_data
-
-
-
+    
         # transformed_data = transform_data(data)
         # print(transformed_data)
